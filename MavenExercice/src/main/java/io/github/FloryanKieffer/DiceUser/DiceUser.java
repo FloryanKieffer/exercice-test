2aacf2d8fe20029886ff8cd1ce810e6802cc8b9b
FloryanKieffer <floryan@mbp-de-floryan.home>
2020-03-18 09:47:16 +0100

    Second commit project

--- MavenExercice/src/main/java/io/github/FloryanKieffer/DiceUser/DiceUser.java
+++ MavenExercice/src/main/java/io/github/FloryanKieffer/DiceUser/DiceUser.java
@@ -6,15 +6,22 @@ public class DiceUser {
 
 	private TripletOfDice threeDice;
 	
-	public TripletOfDice DiceUser() {
+	public DiceUser() {
 		this.threeDice = new TripletOfDice(6,6,6);
 		this.threeDice.rollAllDice();
-		return this.threeDice;
 	}
 	
 	public static void main(String[] args) {
 		DiceUser troisDes = new DiceUser();
-		troisDes.threeDice.getFirstDice()
+		troisDes.threeDice.roll(1);
+		troisDes.threeDice.roll(2);
+		troisDes.threeDice.roll(3);
+		
+		System.out.println(troisDes.threeDice.getFirstDice());
+		System.out.println(troisDes.threeDice.getSecondDice());
+		System.out.println(troisDes.threeDice.getThirdDice());
+		
+		
 			
 
 	}
